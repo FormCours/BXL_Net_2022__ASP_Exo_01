eb56b0b8217cab9a4e3d5710edc77058520c6007
Aurélien S <aurelien.strimelle@bstorm.be>
2022-05-20 13:56:16 +0200

    Ajoutez des fichiers projet.

---
+++ ASP_MVC_Exo_01/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using ASP_MVC_Exo_01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_MVC_Exo_01.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        #region Static Data
+        private IEnumerable<Product> _Products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Pomme",
+                Description = "Un kilo de pomme rouge",
+                Price = 2.50,
+                Reference = "PoRouge01"
+            },
+            new Product()
+            {
+                Id = 2,
+                Name = "Banane",
+                Description = "Un kilo de banane",
+                Price = 5,
+                Reference = "Banana42",
+                Discount= 15
+            },
+            new Product()
+            {
+                Id = 3,
+                Name = "Kiwi",
+                Description = "Kiwi a la piece",
+                Price = 0.99,
+                Reference = "ZeKiwi"
+            },
+            new Product()
+            {
+                Id = 4,
+                Name = "Tomate",
+                Description = "Un kilo de tomate",
+                Price = 3.95,
+                Reference = "TomateLuxe",
+                Discount = 40
+            }
+        };
+        #endregion
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _Products.OrderBy(p => p.Name);
+
+            return View(products);
+        }
+
+        [Route("/Product/Detail/{id}")]
+        public IActionResult Detail(int id)  
+        { 
+            Product jacky = _Products.SingleOrDefault(p => p.Id == id);
+
+            if(jacky is null)
+            {
+                return NotFound();
+            }
+
+            return View(jacky);
+        }
+    }
+}
---
+++ ASP_MVC_Exo_01/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace ASP_MVC_Exo_01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int? Discount { get; set; }
+        public string Reference { get; set; }
+
+        public double? PriceDiscounted
+        {
+            get
+            {
+                return (Discount is null) ? null : Price - (Price * Discount / 100);
+            }
+        }
+    }
+}
